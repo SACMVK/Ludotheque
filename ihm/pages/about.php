7f640ab1c92b65b446c9efb6260c2a78102ffc57
Chachou <Chachou@DESKTOP-OSHB12F.561557n.ac-rennes.fr>
2017-02-24 09:07:18 +0100

    1er commit avec le répertoire de Charlotte -- IHM de la deuxième semaine-- Rien comme classe etc etc--

---
+++ ihm/pages/about.php
@@ -0,0 +1,28 @@
+ 
+<!-- charlotte -- A propos-->
+
+<div class="article-clean">
+        <div class="container">
+            <div class="row">
+                
+                <!-- découpage de la page( col-lg 8 ...etc) par boostrapp afin que ce soir responsive-->
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <div class="intro">
+                        <h1 class="text-center">A Propos</h1><img class="img-responsive" src="ihm/img/pc.jpg"</div>
+                                        
+                    <div class="text">
+                        <p>Sed lobortis mi. Suspendisse vel placerat ligula. ac sem lac. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in
+                            justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
+                        <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac lacus. <strong>Ut vehicula rhoncus</strong> elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit <em>pulvinar dict</em> vel in justo. Vestibulum
+                            ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
+                        <h2>Aliquam In Arcu </h2>
+                        <p>Suspendisse vel placerat ligula. Vivamus ac sem lac. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in justo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+                            posuere cubilia Curae.</p>
+                        
+                        <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae. Suspendisse vel placerat ligula. Vivamus ac sem lac. Ut vehicula rhoncus elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit
+                            pulvinar dictum vel in justo.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </d
---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta charset="UTF-8" />
+    <link rel="stylesheet" href="/ihm/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/ihm/css/MUSA_carousel-extended.css">
+    <link rel="stylesheet" href="/ihm/css/Navigation-with-Button1.css">
+    <link rel="stylesheet" href="/ihm/css/styles.css">
+    
+<title>Ludotheque BTS</title>
+
+<?php require ('./ihm/pages/effets.php'); ?>
+</head>
+
+
+<body>
+
+
+<?php
+include ('ihm/header/header.php');
+
+//require_once('ihm/menus/menuAdmin.php');//
+include ('ihm/footer/footer.php');
+
+
+/* charlotte : empty ne fonctionne pas car il vérifie si $_GET['page']=null, hors $_GET['page'] n'existe pas
+ * in_array ne fonctionne pas non plus, pour une raison indéterminée
+ * isset (is set) vérifie non =null mais vérifie si la variable existe ou pas avec une valeur
+ */ 
+
+
+if (!(isset($_GET['page']))){
+    include('ihm/pages/accueil.php');
+}
+else {
+    include'ihm/pages/'.$_GET['page'].'.php';
+}
+?>
+
+
+    <script src="../ihm/js/boostrap.js"></script>
+    <script src="../ihm/js/jquery-3.1.1.min.js"></script>
+     <script src="ihm/js/jquery.min.js"></script>
+    <script src="ihm/js/bootstrap.min.js"></script>
+    <script src="ihm/js/MUSA_carousel-extended.js"></script>
+
+    </body>
+
+
+</html>
